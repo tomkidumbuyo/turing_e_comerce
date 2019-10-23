8ed931baf4214bde4eeec11abb994b0aba7d9625
tom kidumbuyo <mryoungtommy@gmail.com>
2019-10-23 12:33:41 +0300

    implement all API endpoints and create angular web app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.SERVER_PORT || 8080;
+const port = process.env.PORT || 8080;
 const express = require('express');
 const expressApp = express();
 require('dotenv').config();
